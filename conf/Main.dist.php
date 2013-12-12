979ffcafffd4ed144a296401be2d714206e343a5
Viacheslav Poturaev <nanopeni@gmail.com>
2013-12-12 16:53:20 +0700

    db timezone

--- conf/Main.dist.php
+++ conf/Main.dist.php
@@ -2,4 +2,4 @@
 
 date_default_timezone_set('Asia/Novosibirsk');
 
-Database_Conf::$dsn['default'] = 'mysqli://user:password@localhost/tpogoda?charset=utf8&timezone=Asia/Novosibirsk';
+Database_Conf::$dsn['default'] = 'mysqli://user:password@localhost/table?charset=utf8&timezone=Asia/Novosibirsk';
