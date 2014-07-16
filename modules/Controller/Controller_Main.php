e57ee048d6a38080775badd3815f4c70e5e4a439
Viacheslav Poturaev <nanopeni@gmail.com>
2014-07-16 14:54:56 +0700

    dev mode

--- modules/Controller/Controller_Main.php
+++ modules/Controller/Controller_Main.php
@@ -4,7 +4,13 @@ class Controller_Main {
 
     public static function indexPage() {
         App::view()->setBody(
-            View_Raw::create('Oh, hi! The INTERNET is waiting for a new site. Some <a href="/some-page">page</a>')
+            View_Raw::create(
+                'Oh, hi! The INTERNET is waiting for a new site.
+                Some <a href="/some-page">page</a>
+                <br><br>
+                <a href="/dev">Enable debug mode</a> (try test:test)
+                '
+            )
         );
         App::view()->render();
     }
